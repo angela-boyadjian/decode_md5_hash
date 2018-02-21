ffafee55821fa143918632fe2ee7aea92bf72af2
Angela <angela.martoccia-boyadjian@epitech.eu>
2018-02-21 02:41:17 +0100

    adding nb argv error handling

--- md5_decrypte.py
+++ md5_decrypte.py
@@ -4,14 +4,18 @@ import sys
 import itertools
 import time
 
+
+if (len(sys.argv) < 3):
+	print("Not enough arguments")
+	sys.exit()
 def bruteforce(solution, nb):
 	items = ["a", "b", "c", "d", "e", "f", "g", "h"
 	"i", "j", "h", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 	for p in itertools.permutations(items, nb):
 		str = ''.join(p)
-		hash_object = hashlib.md5(str)
+		hash_object = hashlib.md5(str.encode('utf-8'))
 		if (hash_object.hexdigest() == solution):
-			print(str)
+			print('Password is: {0}'.format(str))
 			break
 bruteforce(sys.argv[1], int(sys.argv[2]))
